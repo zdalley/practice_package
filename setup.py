3cb21f2763590b51e352137dff289592fbe69019
zdalley <zackdalley45@gmail.com>
2024-10-31 10:08:37 -0600

    Added setup.py, requirements.txt to automatically have required packages downloaded

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'zack_practice_package',
+    version = '0.0.1',
+    packages = find_packages()
+    requires = ['numpy', 'pandas']
+)
